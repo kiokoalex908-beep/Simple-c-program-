2e09612bf295a8fa13f9931ba49bf9a060fe4d44
kiokoalex908-beep <kiokoalex908@gmail.com>
2025-09-25 01:34:28 +0300

    Add files via upload

---
+++ Simple c program to prompt user to enter details .c
@@ -0,0 +1,29 @@
+//simple c program
+/*
+Name:Mutunga Alex
+Reg :CT100/G/26278/25
+Description :Details
+*/
+
+#include<stdio.h>
+
+int main (){
+        int height;
+        int phonenumber;
+        int weight;
+        
+        printf("Enter your height:");
+        scanf("%d" ,&height );
+        
+        printf("Enter your Phonenumber:");
+        scanf("%d" ,&phonenumber);
+        
+        printf("Enter your weight:");
+        scanf("%d", &weight );
+        
+        printf("\n your height is %d\n", height );
+        printf("\n your phonenumber is %d\n", phonenumber );
+        printf("your weight is %d kg\n", weight );
+        
+        return 'O';
+        }
