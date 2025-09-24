4b336193a46923b8dd8f4d0abe70ce7d6402ae6b
kiokoalex908-beep <kiokoalex908@gmail.com>
2025-09-25 01:33:27 +0300

    Add files via upload

---
+++ Semester units.c
@@ -0,0 +1,22 @@
+//simple c program 
+/*
+Name: Mutunga Alex 
+Reg:CT100/G/26278/25
+Description semester unit
+*/
+#include <stdio.h>
+
+//main function 
+int main(){
+
+printf("BIT SEMESTER UNITS\n ");
+     printf ("1:Analogue Electronics \n " );
+     printf("2:computer system and Organization \n ");
+     printf("3:Fundamental of ICT \n ");
+     printf("4:Essentials of Accounting\n ");
+     printf("5:Introduction to programming and Algorithms \n ");
+     printf("6:Mathematics for science \n ");
+     printf("7:communication skills and Information letracy \n ");
+     printf("8:Introduction to computer application \n ");
+     return 'o';
+     }
