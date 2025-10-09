75813f4caf74f0fc44882ab1711810647c3938d5
kiokoalex908-beep <kiokoalex908@gmail.com>
2025-10-09 18:46:29 +0300

    Add files via upload

---
+++ Numbers.c
@@ -0,0 +1,18 @@
+/*
+Name:Mutunga Alex
+REG:CT100/G/26278/25
+Description:
+*/
+
+#include<stdio.h>
+int main(){
+int i=100;	
+
+for (i<100;i>=50;--i)
+   printf(" %d\n",i);	
+			
+return 0;	
+}
+
+
+
